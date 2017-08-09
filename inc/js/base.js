dcd8b8129a7e516e0034646888a376110bd80d9a
Connie Cheng <connie.cheng52@gmail.com>
2017-08-09 00:39:02 -0400

    functioning slider

--- inc/js/base.js
+++ inc/js/base.js
@@ -22,10 +22,21 @@
 		},
 		next:function(){
 			//ADD NEXT CODE HERE
+      $(`article:nth-child(${this.props.current_slide})`).addClass('off').removeClass('active')
+      this.props.current_slide += 1;
+      if(this.props.current_slide > this.props.total_slides){
+          this.props.current_slide=1;
 
+          $('article:nth-child(1)').removeClass('off').show().addClass('active').siblings().removeClass('off')
+      } else{
+          $(`article:nth-child(${this.props.current_slide})`).addClass('active').siblings().removeClass('active');
+      }
 		},
 		previous:function(){
 			//ADD PREVIOUS CODE HERE
+      this.props.current_slide-=1;
+      $(`article:nth-child(${this.props.current_slide})`).addClass('prev').removeClass('off').removeClass('active')
+      setTimeout(()=>{$(`article:nth-child(${this.props.current_slide})`).removeClass('prev')},1000)
 		},
 		update:function(){
 			//ADD UPDATE CODE HERE
